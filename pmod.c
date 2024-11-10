51c9f91af5b40ab20a29dee5ed58565c3322fdcd
Vincent Vassallo <vassallov@msoe.edu>
2024-11-10 13:30:49 -0600

    pmod done

--- pmod.c
+++ pmod.c
@@ -1,9 +1,19 @@
-// Name, etc
+/*************************************************************************** */ 
+//Author: Vincent Vassallo
+//Filename: pmod.c
+//Section: 111
+//Description: File to retrieve process information
+//************************************************************************** */
+#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
 
-// pmod.c
-
-int main(int argc, char* argv[])
-{
-
+int main(int argc, char* argv[]) {
+    struct timespec ts; //Declare struct
+    ts.tv_sec = 1; //Set seconds
+    ts.tv_nsec = 837272638; //Set nanoseconds
+    nice(10); //Reduce priotity by 10
+    nanosleep(&ts, NULL); //Sleep for the time in the struct, no remainder needed
+    printf("Sleep finished: Goodbye\n"); //Print exit message
     return 0;
 }
